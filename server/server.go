5a51c28c7843e7362fb19807c73c58a83ccb9047
Renato Angeline <reangeline@MacBook-Pro-de-Renato.local>
2022-11-27 20:23:39 -0300

    fixes - add context and timeout

--- server/server.go
+++ server/server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Dolar struct {
@@ -37,13 +39,33 @@ func NewCotation(cotation string) *Cotation {
 }
 
 func main() {
+	createDB()
 	http.HandleFunc("/cotacao", GetApi)
 	fmt.Println("Server started on port:8080")
 	http.ListenAndServe(":8080", nil)
 }
+func createDB() {
+	db := conn()
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS cotations(cotation TEXT)")
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
+
+func conn() *sql.DB {
+	db, err := sql.Open("sqlite3", "db/dolar.db")
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
 func GetApi(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+
+	c := http.Client{Timeout: time.Millisecond * 200}
+
+	resp, err := c.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	if err != nil {
 		panic(err)
 	}
@@ -61,14 +83,14 @@ func GetApi(w http.ResponseWriter, r *http.Request) {
 		panic(error)
 	}
 
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	db, err := sql.Open("sqlite3", "dolar.db")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
 	cotation := NewCotation(string(body))
-	err = insert(db, cotation)
+	err = insert(cotation)
 	if err != nil {
 		panic(err)
 	}
@@ -77,21 +99,20 @@ func GetApi(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func insert(db *sql.DB, cotation *Cotation) error {
+func insert(cotation *Cotation) error {
 
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS cotations (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, cotation TEXT NOT NULL)")
-	if err != nil {
-		panic(err)
-	}
+	db := conn()
+	defer db.Close()
 
-	stmt, err := db.Prepare("insert into cotations(cotation) values(?)")
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*10)
+	defer cancel()
+
+	_, err := db.ExecContext(ctx, "INSERT INTO cotations(cotation) values(?)", cotation.cotation)
 	if err != nil {
+		fmt.Println("erro")
 		return err
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(cotation.cotation)
-	if err != nil {
-		return err
-	}
+
 	return nil
 }
